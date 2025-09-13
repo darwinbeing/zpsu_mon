45a6529059652fa944c7950a3795cf70791bbeb8
LI Tao <litroncn@gmail.com>
2025-09-12 20:25:42 -0700

    Update

--- src/psu_ctrl.c
+++ src/psu_ctrl.c
@@ -215,8 +215,6 @@ ZBUS_CHAN_DECLARE(psuctrl_data_chan);
 static void psuctrl_work_cb(struct k_work *work);
 static K_WORK_DELAYABLE_DEFINE(psuctrl_work, psuctrl_work_cb);
 
-// static float Gain=1.14;
-static float Gain=1.1723;
 static float volts = 0;
 static float amps = 0;
 static float watts = 0;
@@ -231,7 +229,7 @@ static void send_psuctrl_data_event(void)
         ret=PSUCtrl_readDPS1200Register(7, &val);
         if(!ret) {
                 volts = val;
-                volts = volts*Gain/253.9;
+                volts = volts/256;
         }
         ret=PSUCtrl_readDPS1200Register(8, &val);
         if(!ret) {
